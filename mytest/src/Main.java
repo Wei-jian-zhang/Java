37187d676495b70695c49ae89e440157f5ebe1de
Wei-jian-zhang <74697474+Wei-jian-zhang@users.noreply.github.com>
2021-11-17 23:50:02 +0800

    Change Some code

---
+++ mytest/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        for (int i=0;i<10;i++)
+        {
+            System.out.println(i);
+        }
+        System.out.println("hellow World!");
+    }
+
+}
